6c4c5d101867e826280cc4ef576809e83766d17d
Tonilson Manoel <tonilsonmanoel@gmail.com>
2022-09-28 21:00:28 +0000

    adicionadas capas livro

---
+++ app/src/main/java/com/example/livros2/Forest_Of_Souls.kt
@@ -0,0 +1,11 @@
+package com.example.livros2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Forest_Of_Souls : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forest_of_souls)
+    }
+}
